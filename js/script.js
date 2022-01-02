140f7144fd43bae98d2ddba73b2c8cb3415413af
martellosios <martellosios@bzz.ch>
2022-01-02 13:07:07 +0100

    getEl by ID

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const currencyEl_one = document.getElementById("currency-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_one = document.getElementById("amount-one");
+const amountEl_two = document.getElementById("amount-two");
