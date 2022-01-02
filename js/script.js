3063a96c20ca30fc91bbe31507958b2ad48faf3a
affolterj <affolterj@bzz.ch>
2022-01-02 13:13:27 +0100

    added Events

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const currencyEl_one = document.getElementById("currency-one");
 const currencyEl_two = document.getElementById("currency-two");
 const amountEl_one = document.getElementById("amount-one");
 const amountEl_two = document.getElementById("amount-two");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+currencyEl_one.addEventListener("change", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+amountEl_two.addEventListener("input", calculate);
