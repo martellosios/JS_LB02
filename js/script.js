986df6ddda5743f56ef5e95441df02f2dfb3ecf1
martellosios <martellosios@bzz.ch>
2022-01-02 13:23:49 +0100

    added function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,21 @@ const amountEl_two = document.getElementById("amount-two");
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
+// "Fetch" Wechselkurse Funktion
+
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(
+    "https://v6.exchangerate-api.com/v6/cbb423278d69ba2d1b7071fc/latest/${currencyEl_one}"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
 currencyEl_one.addEventListener("change", calculate);
 currencyEl_two.addEventListener("change", calculate);
 amountEl_one.addEventListener("input", calculate);
