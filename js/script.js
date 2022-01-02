a427a683197aaa63bf8906f55122283650f7b697
martellosios <martellosios@bzz.ch>
2022-01-02 15:25:17 +0100

    test

--- js/script.js
+++ js/script.js
@@ -9,31 +9,32 @@ const swap = document.getElementById("swap");
 // "Fetch" Wechselkurse Funktion
 
 function calculate() {
-    const currency_one = currencyEl_one.value;
-    const currency_two = currencyEl_two.value;
-  
-    fetch(`https://v6.exchangerate-api.com/v6/cbb423278d69ba2d1b7071fc/latest/${currency_one}`)
-      .then((res) => res.json())
-      .then((data) => {
-        //   console.log(data);
-        const rate = data.conversion_rates[currency_two];
-        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-  
-        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-      });
-  }
-  
-  // Event Listeners
-  currencyEl_one.addEventListener('change', calculate);
-  amountEl_one.addEventListener('input', calculate);
-  currencyEl_two.addEventListener('change', calculate);
-  amountEl_two.addEventListener('input', calculate);
-  swap.addEventListener('click', () => {
-    const temp = currencyEl_one.value;
-    currencyEl_one.value = currencyEl_two.value;
-    currencyEl_two.value = temp;
-    calculate();
-  });
-  
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(
+    `https://v6.exchangerate-api.com/v6/cbb423278d69ba2d1b7071fc/latest/${currency_one}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      const rate = data.conversion_rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
+// Event Listeners
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
+swap.addEventListener("click", () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
   calculate();
-  
+});
+
+calculate();
