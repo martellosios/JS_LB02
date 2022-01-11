d46b669698eb19b3dadfa983528861fedcb8ef7b
affolterj <affolterj@bzz.ch>
2022-01-11 16:06:02 +0100

    added comments to js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
+// Währungen abrufen
 const currencyEl_one = document.getElementById("currency-one");
 const currencyEl_two = document.getElementById("currency-two");
+
+// Eingabe abrufen
 const amountEl_one = document.getElementById("amount-one");
 const amountEl_two = document.getElementById("amount-two");
 
+// Wechselkurs abrufen
 const rateEl = document.getElementById("rate");
+
+// Wechselknopf abrufen
 const swap = document.getElementById("swap");
 
 // "Fetch" Wechselkurse Funktion
 
+// Wechselkurs eingabe berechnen
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
+// "Fetch" Wechselkurse Funktion API json 
   fetch(
     `https://v6.exchangerate-api.com/v6/cbb423278d69ba2d1b7071fc/latest/${currency_one}`
   )
