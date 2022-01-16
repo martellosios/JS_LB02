bc274abddc7fe92d0c81d00d2ef05c1dcdf1ef06
martellosios <martellosios@bzz.ch>
2022-01-16 15:14:11 +0100

    push

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-// "Fetch" Wechselkurse Funktion API json 
+  // "Fetch" Wechselkurse Funktion API json
   fetch(
     `https://v6.exchangerate-api.com/v6/cbb423278d69ba2d1b7071fc/latest/${currency_one}`
   )
